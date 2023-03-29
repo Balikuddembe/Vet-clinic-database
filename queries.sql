36d7ccbf6a55be01c9306fd4ed18086e9b73215a
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-03-29 23:12:47 +0300

    Add transactions to queries.sql file

--- queries.sql
+++ queries.sql
@@ -8,3 +8,40 @@ SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered IS true;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- Update table
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT species FROM animals;
+---
+ROLLBACK;
+SELECT species FROM animals;
+---
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT species FROM animals;
+COMMIT;
+---
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+---
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint;
+SELECT * FROM animals;
+---
+UPDATE animals SET weight_kg = weight_kg * -1;
+SELECT * FROM animals;
+---
+ROLLBACK TO SAVEPOINT my_savepoint;
+SELECT * FROM animals;
+---
+UPDATE animals SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+---
